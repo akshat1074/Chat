bd316f2cdb2da387001827fb9d1ea96296b80563
akshat_1074 <valaniakshat1074@gmail.com>
2025-04-14 22:04:44 +0530

    Adding Socketio

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,13 +3,34 @@ import dotenv from "dotenv";
 import messageRoutes from "./routes/auth.route.js"
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors"
 
 dotenv.config()
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+        origin:"http://localhost:5173",
+        credentials:true,
+    })
+)
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
+
+
 const PORT = process.env.PORT;
 
 app.listen(5001,()=>{
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
-import {create} from "zustand"
+import {create} from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set)=>({
     authUser:null,
@@ -30,9 +31,54 @@ export const useAuthStore = create((set)=>({
             const res = await axiosInstance.post("/auth/signup",data);
             set({authUser:res.data})
         }catch{
-            
+            toast.error(error.response.data.message);
+        } finally{
+            set({isSigningUp:false})
         }
-    }
+    },
+
+    login:async(data)=>{
+        set({isLoggingIn:true});
+        try{
+            const res = await axiosInstance.post("/auth/login",data);
+            set({authUser:res.data});
+            toast.success("Logged in successfully");
+
+        
+        }catch(error){
+            toast.error(error.response.data.message);
+        }finally{
+            set({isLoggingIn:false});
+        }
+
+    },
+
+     logout:async()=>{
+        try{
+            await axiosInstance.post("/auth/logout");
+            set({authUser:null});
+            toast.success("Logged out successully");
+        
+        }catch(error){
+            toast.error(error.response.data.message);
+        }
+     },
+
+
+
+
+    updateProfile:async(data)=>{
+        try{
+            await axiosInstance.post("/auth/update-profile",data);
+            set({authUser:res.data});
+            toast.success("Profile updated successfully")
+        } catch(error){
+            console.log(error.response.data.message);
+            toast.error(error.response.data.message);
+        } finally{
+            set({isUpdatingProfile:false});
+        }
+    },
       
 
 
